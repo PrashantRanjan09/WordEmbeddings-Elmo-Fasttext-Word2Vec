ac782f414bc76cf5cd85ae682a6fea99a875d73d
Prashant Ranjan <ranjan.prashant.92@gmail.com>
2018-06-15 03:54:19 +0530

    elmo embeddings added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
 import numpy as np
 from sklearn.model_selection import train_test_split
-from word_embeddings import load_data,prepare_data_for_word_vectors,building_word_vector_model,classification_model,padding_input,prepare_data_for_word_vectors_imdb
+from word_embeddings import load_data,prepare_data_for_word_vectors,building_word_vector_model,\
+classification_model,padding_input,prepare_data_for_word_vectors_imdb,ELMoEmbedding,data_prep_ELMo,Classification_model_with_ELMo
 
 
 def json_to_dict(json_set):
@@ -30,30 +31,39 @@ model_params = json_to_dict(model_params)
 
 '''
 # for imdb data
-x_train,x_test,y_train,y_test = load_data(params_set["vocab_size"],params_set["max_len"])
-sentences,word_ix = prepare_data_for_word_vectors_imdb(x_train)
-model_wv = building_word_vector_model(params_set["option"],sentences,params_set["embed_dim"],
-                                   params_set["workers"],params_set["window"],y_train)
+if params_set["option"]in [0,1,2]:
+    x_train,x_test,y_train,y_test = load_data(params_set["vocab_size"],params_set["max_len"])
+    sentences,word_ix = prepare_data_for_word_vectors_imdb(x_train)
+    model_wv = building_word_vector_model(params_set["option"],sentences,params_set["embed_dim"],
+                                       params_set["workers"],params_set["window"],y_train)
 
-# for other data:
-# put your data in the form of X,y
-'''
-X = ["this is a sentence","this is another sentence by me","yet another sentence for training","one more again"]
-y=np.array([0,1,1,0])
+    # for other data:
+    # put your data in the form of X,y
+    '''
+    X = ["this is a sentence","this is another sentence by me","yet another sentence for training","one more again"]
+    y=np.array([0,1,1,0])
 
-sentences_as_words,sentences,word_ix = prepare_data_for_word_vectors(X)
-print("sentences loaded")
-model_wv = building_word_vector_model(params_set["option"],sentences,params_set["embed_dim"],
-                                   params_set["workers"],params_set["window"],y)
-
-
-print("word vector model built")
-x_train, x_test, y_train, y_test = train_test_split(sentences, y, test_size=params_set["split_ratio"], random_state=42)
-print("Data split done")
-'''
-x_train_pad,x_test_pad = padding_input(x_train,x_test,params_set["max_len"])
+    sentences_as_words,sentences,word_ix = prepare_data_for_word_vectors(X)
+    print("sentences loaded")
+    model_wv = building_word_vector_model(params_set["option"],sentences,params_set["embed_dim"],
+                                       params_set["workers"],params_set["window"],y)
 
 
-model = classification_model(params_set["embed_dim"],x_train_pad,x_test_pad,y_train,y_test,
-                     params_set["vocab_size"],word_ix,model_wv,params_set["trainable_param"])
-print(model.summary())
+    print("word vector model built")
+    x_train, x_test, y_train, y_test = train_test_split(sentences, y, test_size=params_set["split_ratio"], random_state=42)
+    print("Data split done")
+    '''
+    x_train_pad,x_test_pad = padding_input(x_train,x_test,params_set["max_len"])
+
+
+    model = classification_model(params_set["embed_dim"],x_train_pad,x_test_pad,y_train,y_test,
+                         params_set["vocab_size"],word_ix,model_wv,params_set["trainable_param"])
+    print(model.summary())
+
+else:
+    x_train,x_test,y_train,y_test = load_data(params_set["vocab_size"],params_set["max_len"])
+
+    train_text,train_label,test_text,test_label = data_prep_ELMo(x_train,y_train,x_test,y_test,params_set["max_len"])
+
+    model = Classification_model_with_ELMo(train_text,train_label,test_text,test_label)
+    print(model.summary())
--- word_embeddings.py
+++ word_embeddings.py
@@ -2,6 +2,9 @@ import json
 import fasttext
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as hub
+import keras.layers as layers
 from keras.models import Model
 from keras.datasets import imdb
 from gensim.models import Word2Vec
@@ -108,6 +111,37 @@ def prepare_data_for_word_vectors(X):
 
     return sentences_as_words,sentences,word_to_index
 
+def data_prep_ELMo(train_x,train_y,test_x,test_y,max_len):
+
+    INDEX_FROM = 3
+    word_to_index = imdb.get_word_index()
+    word_to_index = {k:(v+INDEX_FROM) for k,v in word_to_index.items()}
+
+    word_to_index["<START>"] =1
+    word_to_index["<UNK>"]=2
+
+    index_to_word = {v:k for k,v in word_to_index.items()}
+
+    sentences=[]
+    for i in range(len(train_x)):
+        temp = [index_to_word[ids] for ids in train_x[i]]
+        sentences.append(temp)
+
+    test_sentences=[]
+    for i in range(len(test_x)):
+        temp = [index_to_word[ids] for ids in test_x[i]]
+        test_sentences.append(temp)
+
+    train_text = [' '.join(sentences[i][:max_len]) for i in range(len(sentences))]
+    train_text = np.array(train_text, dtype=object)[:, np.newaxis]
+    train_label = train_y.tolist()
+
+    test_text = [' '.join(test_sentences[i][:500]) for i in range(len(test_sentences))]
+    test_text = np.array(test_text , dtype=object)[:, np.newaxis]
+    test_label = test_y.tolist()
+
+    return train_text,train_label,test_text,test_label
+
 
 def building_word_vector_model(option,sentences,embed_dim,workers,window,y_train):
     """
@@ -171,8 +205,14 @@ def padding_input(X_train,X_test,maxlen):
     return X_train_pad,X_test_pad
 
 
+def ELMoEmbedding(x):
+    elmo_model = hub.Module("https://tfhub.dev/google/elmo/1", trainable=True)
+    return elmo_model(tf.squeeze(tf.cast(x, tf.string)), signature="default", as_dict=True)["default"]
+
+
+
 def classification_model(embed_dim,X_train_pad,X_test_pad,y_train,y_test,vocab_size,word_index,w2vmodel,
-                         trainable_param):
+                         trainable_param,option):
     """
         Builds the classification model for sentiment analysis
 
@@ -184,6 +224,7 @@ def classification_model(embed_dim,X_train_pad,X_test_pad,y_train,y_test,vocab_s
             word_index =  {dict} word and its indexes in whole of imdb corpus
             w2vmodel = Word2Vec model
             trainable_param = {bool} whether to train the word embeddings in the Embedding layer
+            option = {int} choice of word embedding
 
 
     """
@@ -214,6 +255,25 @@ def classification_model(embed_dim,X_train_pad,X_test_pad,y_train,y_test,vocab_s
     model.compile(loss=model_params["loss"],optimizer=model_params["optimizer"],metrics= model_params["metrics"])
 
     model.fit(X_train_pad,y_train,epochs=model_params["epochs"],batch_size=model_params["batch_size"],validation_data=(X_test_pad,y_test))
+    predictions = model.predict(X_test_pad)
+    predictions = [0 if i<0.5 else 1 for i in predictions]
+    print("Accuracy: ",accuracy_score(y_test,predictions))
+    print("Classification Report: ",classification_report(y_test,predictions))
+
+    return model
+
+def Classification_model_with_ELMo(X_train_pad,y_train,X_test_pad,y_test):
+    input_text = layers.Input(shape=(1,), dtype=tf.string)
+    embed_seq = layers.Lambda(ELMoEmbedding, output_shape=(1024,))(input_text)
+    x = Dense(256,activation ="relu")(embed_seq)
+    preds = Dense(1,activation="sigmoid")(x)
+    model = Model(input_text,preds)
+
+
+    model.compile(loss="binary_crossentropy",optimizer="adam",metrics=["accuracy"])
+
+    model.fit(X_train_pad,y_train,epochs=10,batch_size=512,validation_data=(X_test_pad,y_test))
+
     predictions = model.predict(X_test_pad)
     predictions = [0 if i<0.5 else 1 for i in predictions]
     print("Accuracy: ",accuracy_score(y_test,predictions))
